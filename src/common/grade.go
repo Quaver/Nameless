b120bb9b1e9efe18a72dde2f92a67d524be4e629
Swan <me@swan.moe>
2021-07-01 09:46:01 -0400

    Add helper function to get grade from accuracy

--- src/common/grade.go
+++ src/common/grade.go
@@ -13,3 +13,30 @@ const (
 	GradeSS          = "SS"
 	GradeX           = "X"
 )
+
+func GetGradeFromAccuracy(acc float32, failed bool) Grade {
+	if failed {
+		return GradeF
+	}
+	
+	if acc >= 100 {
+		return GradeX
+	}
+	if acc >= 99 {
+		return GradeSS
+	}
+	if acc >= 95 {
+		return GradeS
+	}
+	if acc >= 90 {
+		return GradeA
+	}
+	if acc >= 80 {
+		return GradeB
+	}
+	if acc >= 70 {
+		return GradeC
+	}
+	
+	return GradeD
+}
