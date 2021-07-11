87653caa36e6dcab972e793c9c28b7313bf3a3bf
Swan <me@swan.moe>
2021-07-11 13:55:38 -0400

    Fetch avatar url from users

--- src/db/user.go
+++ src/db/user.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	"errors"
 	"fmt"
 	"time"
 )
@@ -16,13 +15,14 @@ type User struct {
 	LatestActivity              int64
 	Country                     string
 	CheckedPreviousAchievements bool
+	AvatarURL					string
 }
 
 // GetUserById Fetches a user object by their id
 func GetUserById(id int) (User, error) {
 	query := "SELECT " +
 		"id, steam_id, username, allowed, privileges, usergroups, latest_activity," +
-		"country, checked_previous_achievements " +
+		"country, checked_previous_achievements, avatar_url " +
 		"FROM users WHERE id = ? LIMIT 1"
 
 	var user User
@@ -30,10 +30,10 @@ func GetUserById(id int) (User, error) {
 	err := SQL.QueryRow(query, id).Scan(
 		&user.Id, &user.SteamId, &user.Username, &user.Allowed,
 		&user.Privileges, &user.UserGroups, &user.LatestActivity,
-		&user.Country, &user.CheckedPreviousAchievements)
+		&user.Country, &user.CheckedPreviousAchievements, &user.AvatarURL)
 
 	if err != nil {
-		return User{}, errors.New("failed to fetch user from the database")
+		return User{}, err
 	}
 
 	return user, nil
