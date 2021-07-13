6b5e59349426373d6eb3b87a8ecb7ca1e4465590
Swan <me@swan.moe>
2021-07-13 11:44:15 -0400

    Implement stepping up the ladder achievement

--- src/achievements/achievement_test.go
+++ src/achievements/achievement_test.go
@@ -126,6 +126,25 @@ func TestHumbleBeginnings(t *testing.T) {
 	}
 }
 
+func TestSteppingUpTheLadder(t *testing.T) {
+	user, stats, err := getUser(1, common.ModeKeys4)
+
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	a := NewAchievementSteppingUpTheLadder()
+	ok, err := a.Check(&user, &db.Score{}, &stats)
+
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	if !ok {
+		t.Fatalf("achievement not unlocked")
+	}
+}
+
 func TestCloseAchievement(t *testing.T) {
 	db.CloseSQLConnection()
 }
