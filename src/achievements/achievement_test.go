fda8e9a4ebcf372ce88fd7da4e76af4d31f3a161
Swan <me@swan.moe>
2021-07-15 12:18:03 -0400

    Implement Its Getting Harder achievement

--- src/achievements/achievement_test.go
+++ src/achievements/achievement_test.go
@@ -392,6 +392,25 @@ func TestBeginnersLuck(t *testing.T) {
 	}
 }
 
+func TestItsGettingHarder(t *testing.T) {
+	user, stats, err := getUser(1, common.ModeKeys7)
+
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	a := NewAchievementItsGettingHarder()
+	ok, err := a.Check(&user, &db.Score{}, &stats)
+
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	if !ok {
+		t.Fatalf("achievement not unlocked")
+	}
+}
+
 func TestCloseAchievement(t *testing.T) {
 	db.CloseSQLConnection()
 }
