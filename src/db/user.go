93c5c6f7ed430b127fb5b690dce2b7d41acb7859
Swan <me@swan.moe>
2021-07-22 11:52:38 -0400

    Revert Use sql.NullString for user avatar_url

--- src/db/user.go
+++ src/db/user.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	"database/sql"
 	"fmt"
 	"github.com/Swan/Nameless/src/common"
 	"strconv"
@@ -19,7 +18,7 @@ type User struct {
 	LatestActivity              int64
 	Country                     string
 	CheckedPreviousAchievements bool
-	AvatarURL					sql.NullString
+	AvatarURL					string
 }
 
 // GetUserById Fetches a user object by their id
