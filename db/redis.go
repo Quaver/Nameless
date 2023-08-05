b6c04ffaccc86d09d29894b7fa3262e770c5448c
Swan <me@swan.moe>
2023-08-05 10:27:49 -0400

    Increase redis read/write timeout to 1 minute

--- db/redis.go
+++ db/redis.go
@@ -5,6 +5,7 @@ import (
 	"github.com/Swan/Nameless/config"
 	"github.com/go-redis/redis/v8"
 	log "github.com/sirupsen/logrus"
+	"time"
 )
 
 var Redis *redis.Client
@@ -13,9 +14,11 @@ var RedisCtx context.Context = context.Background()
 // InitializeRedis Initializes a Redis client
 func InitializeRedis() {
 	Redis = redis.NewClient(&redis.Options{
-		Addr:     config.Data.Redis.Address,
-		Password: config.Data.Redis.Password,
-		DB:       config.Data.Redis.DB,
+		Addr:         config.Data.Redis.Address,
+		Password:     config.Data.Redis.Password,
+		DB:           config.Data.Redis.DB,
+		ReadTimeout:  time.Minute,
+		WriteTimeout: time.Minute,
 	})
 
 	log.Info("Successfully connected to Redis!")
